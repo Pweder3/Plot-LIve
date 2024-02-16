aa5b954936b3bc6ff1efdc7bb6d63423bba71f54
Pweder69 <pweder07@charlottesvilleschools.org>
2024-02-15 22:15:50 -0500

    redo of data input

--- src/LiveGraph.py
+++ src/LiveGraph.py
@@ -11,25 +11,27 @@ logging.basicConfig(level=logging.INFO)
 class LiveGraph():
     
     
-    def __init__(self,plotdata,inteactiveMode = True) -> None:
+    def __init__(self,*args,inteactiveMode = True,plot_size = (10,10) ) -> None:
         
+        
+        # TODO: type cast correctly and docstrings
         
         # plotData = [grain,dataNames,overAllName,colors]
         
-        self.fig = plt.figure(figsize=(10,10))
+        self.fig = plt.figure(figsize=plot_size)
+        
         
         self.plots = []
-        for i,data in enumerate(plotdata):
+        for i,data in enumerate(args):
+            
             self.plots.append(Plot(
                                    self.fig,
-                                   self.fig.add_subplot(len(plotdata),1,i+1),
+                                   self.fig.add_subplot(len(args),1,i+1),
                                    data[0],
                                    data[1],
                                    data[2],
                                    data[3] if len(data) > 3 else None
                                    ))
-        
-
         
         self.tick = 0
         plt.show(block = False)
@@ -42,26 +44,27 @@ class LiveGraph():
             # TODO: Allow for non interactive mode with window change detection and then updating after change 
         
         
-    def update(self,y: list[list[int,]] = None ,drawInSequence: bool = True) -> None :
-        
-        
-        
+    def update(self,*args,drawInSequence: bool = True) -> None :
+        if len(args) < len(self.plots) :
+            raise ValueError("No data to plot")
         
         self.tick += 1
         if drawInSequence:
             [plot.restore() for plot in self.plots]
-            [plot.setYData(y[i]) for i, plot in enumerate(self.plots)] if y is not None else None
+            [plot.setYData(args[i]) for i, plot in enumerate(self.plots)] 
             [plot.draw_artist() for plot in self.plots]
             [plot.blit() for plot in self.plots]
+            
         else:
             for i,plot in enumerate(self.plots):
                 plot.restore()
-                plot.setYData(y[i]) if y is not None else None
+                plot.setYData(args[i])
                 plot.draw_artist()
                 plot.blit()
         self.fig.canvas.flush_events()
         
     def updateOnWindowChange(self,event):
+        raise NotImplementedError
         
         for plot in self.plots:
             if event is not None:
@@ -74,6 +77,19 @@ class LiveGraph():
         
             
 
+if __name__ == "__main__":
+    LG = LiveGraph([200,['a','b'],'fig1',["r","b"]],
+                   [50,['a','b'],'fig2',["r","b"]]
+                   )
+    cycleCount = 0
+    start_time = time.time()
+    
+    for j in range(1000):
+        LG.update([1,math.sin(j/100)],
+                  [math.sin(j/100),1]
+                  )
+        cycleCount += 1
+        print(cycleCount/(time.time()-start_time))
         
 
             
--- src/plot.py
+++ src/plot.py
@@ -13,30 +13,27 @@ logging.basicConfig(level=logging.INFO)
 
 class Plot():
     
-    def __init__(self,fig: figure , ax: axes ,grain: int,dataNames: list[int,] , names = list[str],colors = None ) -> None:
-        
-        
-
+    def __init__(self,fig: figure , ax: axes ,grain: int,data_names: list[str] , plot_name: str,colors = None ) -> None:
         self.fig = fig
         self.ax = ax
         self.grain = grain
         
         
-        self.names = names
-        self.color = "r" * len(dataNames) if colors is None else colors
+        self.plot_names = plot_name
+        self.color = "r" * len(data_names) if colors is None else colors
+        # makes all red if no color is given
         
         
-        self.xData = range(grain)
+        self.x_data = range(grain)
+        self.y_data = []
+        self.lines = []
         
-        self.yData = []
-        self.Lines = []
         
-        
-        for i in range(len(dataNames)):
-            self.yData.append([0] * grain ) 
-            ln, = self.ax.plot(self.xData,self.yData[i] , animated=False,label=dataNames[i], color=colors[i])
+        for i in range(len(data_names)):
+            self.y_data.append([0] * grain ) 
+            ln, = self.ax.plot(self.x_data,self.y_data[i] , animated=False,label=data_names[i], color=colors[i])
             
-            self.Lines.append(ln)            
+            self.lines.append(ln)            
         
     
         self.minVal = 0
@@ -46,7 +43,6 @@ class Plot():
         
         self.fig.canvas.draw()
         self.fig.show()
-        
         self.bg = fig.canvas.copy_from_bbox(self.ax.bbox)
         self.fig.canvas.blit(self.ax.bbox)
         self.ax.legend()
@@ -57,39 +53,37 @@ class Plot():
     
     def setYData(self,y:list[int,]): # for loop could be made more readable 
         for i,yVal in enumerate(y):
-            self.yData[i].append(yVal) 
-            self.yData[i].pop(0) 
-            
-            self.Lines[i].set_ydata(self.yData[i]) # every line has its own yData
+            self.y_data[i].append(yVal) 
+            self.y_data[i].pop(0) 
+            self.lines[i].set_ydata(self.y_data[i]) # every line has its own yData
         
         
     def draw_artist(self,minVal:int  = None, maxVal: int = None):
         
-        currMax = max([max(y) for y in self.yData ])
-        currMin = min([min(y) for y in self.yData ])
+        curr_max = max([max(y) for y in self.y_data ])
+        curr_min = min([min(y) for y in self.y_data ])
+
         
-        # TODO: when the graph is more that 10% away from the edges start shrinking it for a cirtain % every call
-        # need to do this to be able to see spikes but also shrink back when they occoer.
         
         if maxVal is None:
-            if self.maxVal <= currMax:
-                self.maxVal = currMax    # sets the maxVal to the highest value ever recorded in the timeline if the data 
+            if self.maxVal <= curr_max:
+                self.maxVal = curr_max    
             else:
                 self.maxVal *= .95
                         
         if minVal is None:
-            if self.minVal >= currMin:
-                self.minVal = currMin  
+            if self.minVal >= curr_min:
+                self.minVal = curr_min  
             else:
                 self.minVal *= .95
             
-        for line in self.Lines:
+        for line in self.lines:
             self.ax.draw_artist(line)
             
         self.ax.axis([0,self.grain,self.minVal,self.maxVal])
             
-        self.ax.set_title(self.names)
-        self.ax.legend()
+        self.ax.set_title(self.plot_names)
+        self.ax.legend()    
         
         
     def blit(self):
